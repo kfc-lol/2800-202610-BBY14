60db910a501b1e8d63534310367a68d3a7ae4d92
jemceii <jemselaaron@gmail.com>
2026-05-05 19:24:46 -0700

    Implemented basic backend functionality in app.js and landing page with ejs modules.

---
+++ app.js
@@ -0,0 +1,77 @@
+require("./utils.js");
+require("dotenv").config();
+
+const express = require("express");
+const session = require("express-session");
+const MongoStore = require("connect-mongo").MongoStore;
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const Joi = require("joi");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Secret Information
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_user_database = process.env.MONGODB_USER_DATABASE;
+const mongodb_session_database = process.env.MONGODB_SESSION_DATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+//
+
+const { database } = include("public/js/databaseConnection");
+const userCollection = database.db(mongodb_user_database).collection("users");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Middleware to serve static files from the 'public' directory
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/app/views");
+
+var mongoStore = MongoStore.create({
+  mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/${mongodb_session_database}`,
+  crypto: {
+    secret: mongodb_session_secret,
+  },
+});
+
+app.use(
+  session({
+    secret: node_session_secret,
+    store: mongoStore,
+    saveUninitialized: false,
+    resave: true,
+  }),
+);
+
+//---------//
+
+// Index
+app.get("/", (req, res) => {
+  res.render("landingpage");
+});
+
+// Color Indexes (example)
+app.get("/login", (req, res) => {});
+
+// Color Pages with Sizes (example)
+app.get("/signup", (req, res) => {});
+
+//---------//
+
+// Error 404 Page
+app.use((req, res) => {
+  res.status(404);
+  res.send("Page not found - 404");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
